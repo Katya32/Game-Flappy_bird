37ac53416045eea236c90940f98f2754c1c439d0
Katya Kondratchuk <68007177+Katya32@users.noreply.github.com>
2021-10-20 18:28:24 +0300

    bird

--- js/index.js
+++ js/index.js
@@ -1,85 +1,93 @@
-const canvas = document.querySelector("#canvas");
-const header = document.querySelector(".header");
-const context = canvas.getContext("2d");
-
-const bird = new Image();
-const background = new Image();
-const foreground = new Image(); 
-const pipeUp = new Image(); 
-const pipeBottom = new Image(); 
-
-bird.src = "img/bird.png";
-background.src = "img/bg.png";
-foreground.src = "img/fg.png";
-pipeUp.src = "img/pipeUp.png"; 
-pipeBottom.src = "img/pipeBottom.png";
-
-const fly = new Audio(); 
-const score_audio = new Audio(); 
-
-fly.src = "audio/fly.mp3"; 
-score_audio.src = "audio/score.mp3";
-
-const gap = 90;
-const gravity = 1.5;
-let positionX = 10;
-let positionY = 150;
-let score = 0;
-
-const pipe = [];
-pipe[0] = {
-    x: canvas.width,
-    y: 0,
-}
-
-function draw() {
-    context.drawImage(background, 0, 0);
-    for(let i=0; i<pipe.length; i+=1) {
-        context.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-        context.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-        pipe[i].x -=1;
-        if(pipe[i].x == 125) {
-        pipe.push({
-        x : canvas.width,
-        y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-        });
-        }
-         // Отслеживание прикосновений
-         if(positionX + bird.width >= pipe[i].x
-         && positionX <= pipe[i].x + pipeUp.width
-         && (positionY <= pipe[i].y + pipeUp.height
-         || positionY + bird.height >= pipe[i].y + pipeUp.height + gap) || positionY + bird.height >= canvas.height - foreground.height) {
-         location.reload(); // Перезагрузка страницы
-         }
-         
-          if(pipe[i].x == 5) {
-          score++;
-          score_audio.play();
-          }
-
-          if(score === 5) {
-            header.textContent ="You win!!!"
-            }
-         
-        }
-        context.drawImage(foreground, 0, canvas.height-foreground.height);
-        context.drawImage(bird, positionX, positionY);
-        
-        positionY += gravity
-        
-        context.fillStyle = "#000";
-        context.font = "24px Verdana";
-        context.fillText("Счет: " + score, 10, canvas.height - 20);
-     
-    requestAnimationFrame(draw);
-}
-
-pipeBottom.onload = draw;
-
-const moveUp = () => {
-positionY -=25;
-fly.play()
-}
-
-document.addEventListener("keydown", moveUp);
+var cvs = document.getElementById("canvas");  
+var ctx = cvs.getContext("2d");  
+ 
+var bird = new Image();  
+var bg = new Image();  
+var fg = new Image();  
+var pipeNorth = new Image();  
+var pipeSouth = new Image();  
+  
+bird.src = "../img/bird.png";  
+bg.src = "../img/bg.png";  
+fg.src = "../img/fg.png";  
+pipeNorth.src = "../img/pipeUp.png";  
+pipeSouth.src = "../img/pipeBottom.png";  
+  
+var gap = 85;  
+var constant;  
+  
+var bX = 10;  
+var bY = 150;  
+  
+var gravity = 1.5;  
+  
+var score = 0;  
+  
+var fly = new Audio();  
+var scor = new Audio();  
+  
+fly.src = "sounds/fly.mp3";  
+scor.src = "sounds/score.mp3";  
+  
+document.addEventListener("keydown",moveUp);  
+  
+function moveUp(){  
+    bY -= 25;  
+    fly.play();  
+}  
+  
+var pipe = [];  
+  
+pipe[0] = {  
+    x : cvs.width,  
+    y : 0  
+};  
+  
+function draw(){  
+      
+    ctx.drawImage(bg,0,0);  
+      
+      
+    for(var i = 0; i < pipe.length; i++){  
+          
+        constant = pipeNorth.height+gap;  
+        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);  
+        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);  
+               
+        pipe[i].x--;  
+          
+        if( pipe[i].x == 125 ){  
+            pipe.push({  
+                x : cvs.width,  
+                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height  
+            });   
+        }  
+          
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){  
+            location.reload();
+        }  
+          
+        if(pipe[i].x == 5){  
+            score++;  
+            scor.play();  
+        }  
+          
+          
+    }  
+  
+    ctx.drawImage(fg,0,cvs.height - fg.height);  
+      
+    ctx.drawImage(bird,bX,bY);  
+      
+    bY += gravity;  
+      
+    ctx.fillStyle = "#000";  
+    ctx.font = "20px Verdana";  
+    ctx.fillText("Score : "+score,10,cvs.height-20);  
+      
+    requestAnimationFrame(draw);  
+      
+}  
+  
+draw();  
 
